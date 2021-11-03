bf5b664f4cf5d44591eaa74d391db6953d60552d
Hessa Aloraifej <aloraifej.hessa@gmail.com>
2021-11-03 16:33:46 +0300

    SecondVersion

---
+++ app/src/main/java/com/example/timer/TimerDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.timer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TimerDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timer_details)
+    }
+}
